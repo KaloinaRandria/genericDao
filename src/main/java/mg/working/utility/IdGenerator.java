7ce2b80aeca591ef0a0704d06eea46fda0506c99
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-05-12 10:04:51 +0300

    Update IdGenerator.java

--- src/main/java/mg/working/utility/IdGenerator.java
+++ src/main/java/mg/working/utility/IdGenerator.java
@@ -8,28 +8,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class IdGenerator {
-    Connection connection = null;
-    public String generateId(String prefix , String sequenceName){
-        String toReturn = "";
-        try {
-            if (connection == null) {
-                this.connection = Connect.dbConnect();
-            }
-            String query = "SELECT nextval(?)";
-            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
-            preparedStatement.setString(1,sequenceName);
-            ResultSet resultSet = preparedStatement.executeQuery();
+    public String generateId(String prefix, String sequenceName) {
+    String toReturn = "";
+    String query = "SELECT nextval(?)";
+
+    try (Connection connection = Connect.dbConnect();
+         PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+        preparedStatement.setString(1, sequenceName);
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
             if (resultSet.next()) {
                 toReturn = prefix + String.format("%04d", resultSet.getInt(1));
             } else {
-                throw new SQLException("Impossible de recuperer la prochaine valeur de la sequence");
+                throw new SQLException("Impossible de récupérer la prochaine valeur de la séquence");
             }
-            resultSet.close();
-            preparedStatement.close();
-            this.connection.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
-        return toReturn;
+    } catch (SQLException e) {
+        throw new RuntimeException("Erreur SQL lors de la génération de l'ID", e);
     }
+    return toReturn;
+}
 }
